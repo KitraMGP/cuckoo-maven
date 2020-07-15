311b69b9674d27249e2bedc0f0b25a05e6c34841
zi-jing <3056894830@qq.com>
2020-07-15 21:38:57 +0800

    更新主页

--- maven/main.js
+++ maven/main.js
@@ -18,7 +18,6 @@ function copyRepoLink() {
 }
 
 function init() {
-    updateElementPositions();
     setRepolistToolbarDescDefault();
     fetchArtifactsJson();
 }
@@ -47,6 +46,7 @@ function fetchArtifactsJson() {
 
 function parseArtifactJson(content) {
     updateStatus("解析中，请稍候...");
+    clearArtifactList();
     var artifactsJson = JSON.parse(content).artifacts;
     window.__APP_STATE__.jsonCache = content;
     for (i = 0; i < artifactsJson.length; i++) {
@@ -62,30 +62,26 @@ function parseArtifactJson(content) {
         trArtifactItem.appendChild(tdName);
         trArtifactItem.appendChild(tdGroup);
         trArtifactItem.appendChild(tdDesc);
-        trArtifactItem.appendChild(createArrowDownTd());
+        trArtifactItem.appendChild(createArrowEnterTd());
         trArtifactItem.setAttribute("artifactIndex", i);
         document.getElementById("repotable").appendChild(trArtifactItem);
-        updateElementPositions();
     }
-    document.getElementById("repotable-placeholder").remove();
-    updateElementPositions();
+    if (document.getElementById("repotable-placeholder") != null)
+        document.getElementById("repotable-placeholder").remove();
     updateStatus("");
 }
 
 function errorFetchingArtifactJson() {
     document.getElementById("repotable").remove();
-    updateElementPositions();
     updateStatus("无法获取artifacts.json，请检查你的网络环境");
 }
 
 function errorParsingArtifactJson() {
     document.getElementById("repotable").remove();
-    updateElementPositions();
     updateStatus("artifact.json语法错误，请联系管理员修复");
 }
 
 function updateStatus(status) {
-    updateElementPositions();
     var hStatus = document.getElementById("repolist-load-status");
     hStatus.innerText = status;
 }
@@ -95,15 +91,17 @@ function clearArtifactList() {
     for (i = 0; i < artifacts.length; i++) {
         artifacts[i].remove();
     }
-    updateElementPositions();
 }
 
 function addPlaceHolderArtifact() {
     document.getElementById("repotable").appendChild(window.__APP_STATE__.placeHolder);
-    updateElementPositions();
 }
 
 function refreshArtifactList() {
+    if (window.__APP_STATE__.isShowingArchives) {
+        document.getElementById("artifacttable").remove();
+        window.__APP_STATE__.isShowingArchives = false;
+    }
     if (document.getElementById("repotable") == null) {
         document.getElementById("repolist-block").appendChild(window.__APP_STATE__.defaultRepoTable);
         fetchArtifactsJson();
@@ -112,12 +110,15 @@ function refreshArtifactList() {
         addPlaceHolderArtifact();
         fetchArtifactsJson();
     }
-    updateElementPositions();
 }
 
 function back() {
+    if (document.getElementById("artifacttable") != null) {
+        document.getElementById("artifacttable").remove();
+    }
     if (window.__APP_STATE__.isShowingArchives) {
         clearArtifactList();
+        createRepoTable();
         addPlaceHolderArtifact();
         parseArtifactJson(window.__APP_STATE__.jsonCache);
         window.__APP_STATE__.isShowingArchives = false;
@@ -128,16 +129,9 @@ function back() {
 
 function enterArtifact(artifactIndex) {
     console.log(artifactIndex);
-}
-
-function updateElementPositions() {
-    var eRepolistBlock = document.getElementById("repotable");
-    var eRepolistToolbarDesc = document.getElementById("repotable-toolbar-desc");
-    if (eRepolistBlock != null && eRepolistToolbarDesc != null) {
-        var maxWidth = eRepolistBlock.clientWidth;
-        var elemWidth = maxWidth - 36 - 32 - 5;
-        eRepolistToolbarDesc.style.setProperty("width", elemWidth + "px");
-    }
+    window.__APP_STATE__.isShowingArchives = true;
+    document.getElementById("repotable").remove();
+    createArtifactTable();
 }
 
 function setRepolistToolbarDesc(desc) {
@@ -145,7 +139,6 @@ function setRepolistToolbarDesc(desc) {
     if (eRepolistToolbarDesc != null) {
         eRepolistToolbarDesc.innerText = desc;
     }
-    updateElementPositions();
 }
 
 function setRepolistToolbarDescDefault() {
@@ -156,7 +149,7 @@ function appendArrowDownElement(parent) {
     parent.innerHTML = parent.innerHTML + '<svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3188" width="36" height="36"><path d="M434.944 790.624l-45.248-45.248L623.04 512l-233.376-233.376 45.248-45.248L713.568 512z" fill="#ffffff" p-id="3189"></path></svg>';
 }
 
-function createArrowDownTd() {
+function createArrowEnterTd() {
     var e = document.createElement("td");
     appendArrowDownElement(e);
     e.setAttribute("class", "repotable-arrow-down");
@@ -167,6 +160,10 @@ function createArrowDownTd() {
     return e;
 }
 
-function createArtifactTable(artifactsJson) {
+function createArtifactTable() {
+    document.getElementById("repolist-block").innerHTML += '<table id="artifacttable"><tr><th>版本</th><th>Minecraft版本</th><th>发布日期</th></tr></table>';
+}
 
+function createRepoTable() {
+    document.getElementById("repolist-block").appendChild(window.__APP_STATE__.defaultRepoTable);
 }
